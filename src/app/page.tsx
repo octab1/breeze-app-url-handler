c83c2558f72a9ea4e809d7ea2b78c51a44dcb748
octab1 <octavio@nolte.io>
2024-10-25 16:19:22 -0300

    Fix url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
+'use client'
+
 import Image from 'next/image'
+import { redirect } from 'next/navigation'
 import styles from './page.module.css'
 
 export default function Home() {
+  if (typeof window !== 'undefined') {
+    const userAgent = navigator.userAgent
+
+    if (/android/i.test(userAgent)) {
+      redirect(
+        'https://play.google.com/store/apps/details?id=com.breeze.dental.app'
+      )
+    } else if (/iPad|iPhone|iPod/.test(userAgent)) {
+      redirect('https://apps.apple.com/mx/app/breeze-oral-care/id6479655092')
+    } else {
+      redirect('https://breezeoralcare.com/')
+    }
+  }
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
